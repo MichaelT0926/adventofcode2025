9fee0c2a3e31e3dce7d330b027cc9cb09cdf03f6
Michael Taylor <michaelt0926@gmail.com>
2025-12-01 17:22:25 -0500

    Fixed solution, added comments for day1 puzzle2

--- Day 1/Day1_Puzzle2_Lock-Rotation-Expanded.py
+++ Day 1/Day1_Puzzle2_Lock-Rotation-Expanded.py
@@ -4285,15 +4285,20 @@ def rotate(sequence_list, dial_start):
         i = int(i.replace(i[0], "", 1))
 
         #Logic for addition or subtraction
+        prev_dial = dial
+
         if addition == True:
             dial = dial + i
         elif addition == False:
             dial = dial - i
 
         #Logic for keeping the number in scope
-        if dial > 99:
+        if dial >= 100:
             dial, counter = upper_correction(dial, counter)
-        elif dial < 0:
+        elif dial <= -1:
+            #Increments counter for all cases of underflow, except for those caused by dial starting at 0 this rotation
+            if prev_dial > 0:
+                counter+=1
             dial, counter = lower_correction(dial, counter)
 
         #When the dial is equal to 0, add 1 to the count
@@ -4302,23 +4307,39 @@ def rotate(sequence_list, dial_start):
     return counter
 
 def upper_correction(dial, counter):
+    #Function to correct an overflow
+
+    #Init variables
     string_of_int = str(dial)
     length_of_string = len(string_of_int)
     counter_addition = ""
 
+    #Converted the int to a str to remove overflow, keeping removed digits to add to the counter
     while length_of_string > 2:
         counter_addition += string_of_int[0]
         string_of_int = string_of_int.replace(string_of_int[0],"",1)
         length_of_string = len(string_of_int)
 
+    #If the dial lands on 0 after correction, removes a count as it will be accounted for in rotate function
+    if int(string_of_int) == 0:
+        counter -= 1
+
     counter += int(counter_addition)
     return (int(string_of_int), counter)
 
 def lower_correction(dial, counter):
+    #Function for correcting underflow
+
+    #Adds 100 as many times as needed to remove underflow, adds 1 to counter for every pass over 0
     while dial < 0:
         if dial < -99:
             counter += 1
         dial += 100
+
+    #If the dial lands on 0 after correction, removes a count as it will be accounted for in rotate function
+    if dial == 0:
+        counter -= 1
+
     return (dial, counter)
 
 main()
