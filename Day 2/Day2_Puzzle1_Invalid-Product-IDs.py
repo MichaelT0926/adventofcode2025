54329e6b751a84ef11ad5eb26bd5631db0375b71
Michael Taylor <michaelt0926@gmail.com>
2025-12-02 14:58:47 -0500

    Started day2 puzzle 1, init variables and broke down my process into functions

---
+++ Day 2/Day2_Puzzle1_Invalid-Product-IDs.py
@@ -0,0 +1,15 @@
+def main():
+    ranges_string = ''
+    ranges_list = string_to_list(ranges_string)
+    ranges_two_d_list = list_to_two_d_list(ranges_list)
+
+def string_to_list(ranges_string):
+    #TO DO: Convert initial string to an array to separate the ranges
+
+def list_to_two_d_list(ranges_list):
+    #TO DO: Turn each range into a two dimensional list to easily access the min and max later
+
+def check_validity(ranges_two_d_list):
+    #TO DO: Add logic to loop through and check each number in each range against the challenge criteria
+
+main()
